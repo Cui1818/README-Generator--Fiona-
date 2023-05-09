18fcff06fe7d81201fd0876ea45cf1142f25252c
Cui1818 <125406452+Cui1818@users.noreply.github.com>
2023-05-09 22:10:09 +0100

    Adding index.js

--- index.js
+++ index.js
@@ -10,17 +10,46 @@ const questions = [
     name:"Title"
 },{
     type:"input",
-    message:"What is the project "
+    message:"the  descriptionf your project.",
+    name :"Description"
+},{ 
+    type:"input",
+    message:"Table of contents.",
+     name:"Table of contents",
+},{ type:"input",
+    message:"What tool do you installation ?",
+    name:"Installation",
+},{ type:"input",
+    message:"What license do you have ?",
+    name:"License",
+},{ type:"input",
+    message:"what is the contributing?",
+    name:"contirbuting",
+},{ type: "input",
+     message:"What test do you have ?",
+    name:"Tests",
+},{ type:"confirm",
+   message:"Do you have any question?",
+   name:"qestion",
 }
+
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+fs.writeFileSync(
+ path.join(__dirname,"app",fileName),   
+)
 }
 
 // function to initialize program
 function init() {
+inquirer.prompt(questions)
+.then((answerObj) =>{
+var data = generateMarkdown(answerObj);
+writeToFile("READMe.html",data );
 
+})
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+    var question = "flase";
+    if(data.question){
+        question = "ture"
+    }
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <title>Document</title>
+  </head>
+  <body>
+      <div class="container row">
+          <div class="col-4"></div>
+          <div class="col-4">
+              <div class="card">
+                  <div class="card-body">
+                      <h5 class="card-title">${data.title}</h5>
+                      <p class="card-text">Is ${data.description} years old</p>
+                      <p class="card-text">Favorite color is ${data.tablet}</p>
+                      <p class="card-text">Is ${emotion}</p>
+                  </div>
+              </div>
+          </div>
+          <div class="col-4"></div>
+      </div>
+  </body>
+  </html>
 `;
 }
+
+
 
 module.exports = generateMarkdown;
